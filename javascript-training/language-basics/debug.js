e916cf9c754af7a4f0fc424b9fd5a3699330ebcc
Sivakumar Madduru <sivakumarmadduru4@gmail.com>
2026-04-09 22:57:14 +0530

    message

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,18 @@
+console.log("executing line 1 ");
+console.log("executing line 2 ");
+console.log("executing line 23");
+console.log("executing line1 ");
+sumOfNumbers(5,10);
+console.log("executing line1 ");
+
+function sumOfNumbers(a,b)
+{
+c=a+b;
+console.log(c);
+
+}
+
+
+
+
+
---
+++ javascript-training/language-basics/hellow-world.js
@@ -0,0 +1,6 @@
+console.log("hello world!");
+console.error("This is error message");
+console.warn("This is warning message");
+console.info("This is info message");
+console.log("I am  learning Javascript ");
+
